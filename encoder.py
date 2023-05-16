8396c02487cdd53e0179f3ddd64a5ba1bec3638e
golapaduck <golapaduck29@naver.com>
2023-05-17 04:18:40 +0900

    회전자 구현까지 성공, roterFunc 1회 실행되면 회전하는 것부터 구현

--- encoder.py
+++ encoder.py
@@ -1,7 +1,8 @@
 import random
 import string
 
-#아스키코드에서 65~90: 대문자, 97~122: 소문자
+#아스키코드에서 65~90: 대문자
+# ' '는 27번째, .은 28번째
 # 원하는 table 모양notch는 회전자 목적, value는 변환목적
 
 # tables =[
@@ -28,7 +29,7 @@ def parser(key):
 
 #setting: 회전판 순서, 회전판 초기 세팅, [플러그보드 설정]
 
-def Setting(plugNum=False):
+def setting(plugNum=False):
     #table 세팅
     tables = list()
     f = open('./tables.txt','r')
@@ -57,18 +58,20 @@ def Setting(plugNum=False):
             order = random.randint(1,8)
         buffer.append(order)
 
-        prime= random.randint(1,26)
+        prime= random.randint(1,28)
         roter.append({'order':order, 'prime': prime})
 
     #plug 세팅
     plug = list()
 
-    if(plugNum > 0):
+    if(14 > plugNum > 0):
         plug = random.sample(string.ascii_uppercase,plugNum*2)
+    elif(plugNum > 13):
+        plug = random.sample(string.ascii_uppercase,26)
 
     #setting 선언
     setting ={
-        'tables': tables,
+        'table': tables,
         'roter': roter,
         'plug': plug,
     }
@@ -102,28 +105,92 @@ def toKey(setting):
     
     return key
 
+def plugBoard(msg, plug):
+    count = 0
+    plugs = plug
+    data = msg
 
-def encoding(msg, setting):
+    for word in data:
+        if(word in plugs):
+            index = plugs.index(word)
 
-
-    data = list(msg)
-
-    for roter in roters:
-        code = data
-        table = list(tables[roter]['value'])
-        for i in range(len(code)):
-            if(code[i] ==' '):
-                continue
+            if(index%2==1):
+                data[count] = plugs[index-1]
             else:
-                index = ord(code[i]) - 65
-                if(index <= 25):
-                    code[i] = table[index]
-                else:
-                    code[i] = table[index - 32].lower()
-        data = code
+                data[count] = plugs[index+1]
+        count = count + 1
     
-    print(data)
+    return data
+
+def roterFunc(word,table):
+    data = word
+    buffer = table
+
+    if(data == ' '):
+        data = buffer[26]
+    elif(data == '.'):
+        data = buffer[27]
+    else:
+        index = ord(data) - 65
+        data = buffer[index]
+    return data
+
+def mech(msg,tables,roters):
+    use = list()
+    data = msg
+
+    #use 리스트에 초기값 저장
+    for roter in roters:
+        index = roter['order'] - 1
+        buffer = list(tables[index]['value'])
+        
+        #notch를 찾기 편하게 알파벳으로 치환
+        notch = tables[index]['notch']
+        no_char = buffer[int(notch)]
+
+        #prime 값으로 table 재구축
+        for i in range(roter['prime']):
+            buffer.insert(0,buffer.pop())
+
+        ready = {'table':buffer, 'notch': no_char}
+
+        use.append(ready)
+
+    #회전자 구동부분
+    code = list(data)
+    new = list()
+
+    #회전자 1회 구동
+    for word in code:
+        newWord = word
+
+        for table in use:
+            newWord = roterFunc(newWord, table['table'])
+        
+        new.append(newWord)
+        
+        
+
+    code = new
 
 
-#encoding('test Text',1)
-print(Setting(1))
+    data = ''.join(s for s in code)
+
+    return data
+        
+def encoding(msg, setting):
+    
+    set = setting
+    data = list(msg)
+    
+    #print(set)
+
+    data = plugBoard(data,set['plug'])
+
+    print(mech(msg, set['table'],set['roter']))
+
+    #return ''.join(s for s in data)
+
+code = encoding('HELLO',setting(4))
+
+print(code)
