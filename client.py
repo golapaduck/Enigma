526e104c88c2336cc5661d1e9d3aa3e505a55ea1
golapaduck <golapaduck29@naver.com>
2023-05-27 03:02:24 +0900

    업로드, 다운로드 추가

--- client.py
+++ client.py
@@ -7,14 +7,73 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
     while True:
-        msg = input("문장을 입력하세요:")
-        if(msg.find('/') == -1):
-            msg = msg+'/'
-        if not msg.strip():
-            print("입력값이 잘못되었습니다.")
-            continue
+        print('-----------------------------------')
+        print('모드를 선택하세요.')
+        print('(0:종료, 1:업로드, 2:다운로드)')
+        print('-----------------------------------')
+        msg = input('입력) ')
+        print('-----------------------------------')
+
         s.sendall(msg.encode('utf-8'))
-        data = s.recv(1024).decode('utf-8')
-        print(f'\n서버){data}')
-        if data == "종료":
+
+        data = (s.recv(1024).decode('utf-8').split())
+        if data[0] == "종료":
+            print('서버)종료합니다.')
+            print('-----------------------------------')
             break
+        
+        elif data[0] == "업로드":
+            print('서버)업로드합니다.')
+            print('문장을 입력하세요. ')
+            print('-----------------------------------')
+            while True:
+                msg = input('입력) ')
+                print('-----------------------------------')
+                s.sendall(msg.encode('utf-8'))
+
+                txt = list(s.recv(1024).decode('utf-8').split('/'))
+                if(txt[0]=='재송신'):
+                    print('다시 입력해주세요.')
+                    print('-----------------------------------')
+                    continue
+                else:
+                    break
+
+            print(f'암호:{txt[0]}')
+            print(f'키:{txt[1]}')
+
+            continue
+
+        elif data[0] == "다운로드":
+            if data[1] == '0':
+                print('서버)업로드된 암호가 없습니다.')
+                continue
+
+            print('서버)다운로드합니다.')
+            print('-----------------------------------')
+
+            while True:
+                print(f'총 {data[1]}개의 암호가 있습니다.')
+                print('다운로드 할 암호를 고르세요.')
+                print('-----------------------------------')
+                msg = input('입력) ')
+
+                if(msg > data[1] or msg == '0'):
+                    print('다시 입력해주세요.')
+                    print('-----------------------------------')
+                    continue
+                else:
+                    break
+
+            s.sendall(msg.encode('utf-8'))
+
+            txt = list(s.recv(1024).decode('utf-8').split('/'))
+            
+            print('-----------------------------------')
+            print(f'암호:{txt[0]}')
+            print(f'키:{txt[1]}')
+            continue
+
+        elif data[0] == "재송신":
+            print('다시 입력해주세요.')
+            continue
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import encoder
 
 HOST = ''
 PORT = 9150
+MODE = [0,1,2]
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -13,6 +14,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     readsocks = [s]
     ids = {}
     id = 1
+    code_list = []
 
     while True:
         readables, writeables, excpetions = select.select(readsocks, [], [])
@@ -25,25 +27,44 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 id = id + 1
             else:  # 이미 접속한 클라이언트의 요청
                 conn = sock
-                data = conn.recv(1024).decode('utf-8')
-                print(f'데이터:{data}')
+                code = conn.recv(1024).decode('utf-8')
+                print(f'user{ids[conn]}: {code}')
 
-                try:
-                    msg = list(data.split('/'))
-                    code = msg[0]
-                    if(len(msg)==2):
-                        key = msg[1]
-                    else:
-                        key=''
-                except ValueError:
-                    conn.sendall(f'입력값이 올바르지 않습니다:{data}'.encode('utf-8'))
-                    continue
-
-                if code == '0' or code == '종료' and key =='':
+                if code == '0' or code == '종료':
                     conn.sendall(f"종료".encode('utf-8'))
+                    print(f'user{ids[conn]} 종료')
                     conn.close()
                     readsocks.remove(conn)  # 클라이언트 접속 해제시 readsocks에서 제거
-                elif key != '':
-                    conn.sendall(('복호화 결과:' + encoder.encoding(code,key = key)).encode('utf-8'))
-                elif code:
-                    conn.sendall(('암호화 결과:' + encoder.encoding(code)).encode('utf-8'))
+
+                elif code == '1' or code == '업로드':
+                    conn.sendall(f"업로드".encode('utf-8'))
+                    
+                    while True:
+                        data = conn.recv(1024).decode('utf-8')
+                        try:
+                            data = data.upper()
+                            msg = encoder.encoding(data)
+                            conn.sendall(msg.encode('utf-8'))
+                            code_list.append(msg)
+                            break
+                        except:
+                            conn.sendall(f'재송신/'.encode('utf-8'))
+                            continue    
+                
+                elif code == '2' or code == '다운로드':
+                    conn.sendall(f"다운로드 {len(code_list)}".encode('utf-8'))
+                    
+                    if(len(code_list) == 0):
+                        break
+                    index = conn.recv(1024).decode('utf-8')
+
+                    try:
+                        conn.sendall(f"{code_list.pop(index - 1)}".encode('utf-8'))
+
+                    except:
+                        conn.sendall(f"{code_list.pop(0)}".encode('utf-8'))
+                    continue
+
+                else:
+                    conn.sendall(f'재송신'.encode('utf-8'))
+                    continue
